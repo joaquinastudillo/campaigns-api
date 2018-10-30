950a804ecd72bebfce129e9496f83ec6d6180bc9
Jereu <jastudim@outlook.com>
2018-10-30 18:20:59 +0000

    add hot fix to roles migration table

--- database/migrations/2018_10_25_195713_create_roles_table.php
+++ database/migrations/2018_10_25_195713_create_roles_table.php
@@ -16,7 +16,7 @@ class CreateRolesTable extends Migration
         Schema::create('roles', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('slug')->unique();
+            $table->string('slug');
             $table->text('permissions')->nullable('{}');
         });
     }
